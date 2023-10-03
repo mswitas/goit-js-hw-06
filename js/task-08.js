e75ddf509678485a93ad8442817e4f94d1869d64
mswitas <mswitas@gmail.com>
2023-10-03 15:06:00 +0200

    add task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const values = {};
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email === "" || password === "") {
+        return alert("Proszę wypełnić wszystkie pola");
+    } 
+    
+    values.email = email;
+    values.password = password;
+    form.reset();
+    return console.log(values);
+}
